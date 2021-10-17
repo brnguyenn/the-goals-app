0567c91a5d44c44bf83fe2ecda7013d5cf5332bd
Brandi Nguyen <brnguyen@scentregroup.com>
2021-10-17 20:59:50 +1100

    chore: set up configs and packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        hero: "url(https://images.unsplash.com/photo-1509099836639-18ba1795216d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1530&q=80)",
+      },
+    },
   },
   variants: {
     extend: {},
